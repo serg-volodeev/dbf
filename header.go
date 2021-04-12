79d5481b65364c5e321aa77699f6220c2aa1a595
serg-volodeev <serg-volodeev@gmail.com>
2021-04-12 16:10:14 +0300

    Modified date

--- header.go
+++ header.go
@@ -1,5 +1,7 @@
 package xbase
 
+import "time"
+
 type header struct {
 	Id         byte
 	ModYear    byte
@@ -16,6 +18,21 @@ type header struct {
 func newHeader() *header {
 	h := &header{}
 	h.Id = dbfId
-	// h.setModDate(time.Now())
+	h.setModDate(time.Now())
 	return h
+}
+
+// Modified date
+
+func (h *header) modDate() time.Time {
+	year := int(h.ModYear) + 1900
+	month := time.Month(h.ModMonth)
+	day := int(h.ModDay)
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+func (h *header) setModDate(d time.Time) {
+	h.ModYear = byte(d.Year() - 1900)
+	h.ModMonth = byte(d.Month())
+	h.ModDay = byte(d.Day())
 }
--- header_test.go
+++ header_test.go
@@ -2,16 +2,22 @@ package xbase
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func currentDate() time.Time {
+	y, m, d := time.Now().Date()
+	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+}
 
 func TestNewHeader(t *testing.T) {
 	h := newHeader()
 
 	require.Equal(t, byte(0x03), h.Id)
-	// require.Equal(t, uint32(0), h.RecCount)
+	require.Equal(t, uint32(0), h.RecCount)
 	// require.Equal(t, -1, h.fieldCount())
-	// require.Equal(t, uint16(0), h.RecSize)
-	// require.Equal(t, currentDate(), h.modDate())
+	require.Equal(t, uint16(0), h.RecSize)
+	require.Equal(t, currentDate(), h.modDate())
 }
