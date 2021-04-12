caa2977ecc8872009512c51d88ca20a8ad12c0dd
serg-volodeev <serg-volodeev@gmail.com>
2021-04-12 14:18:05 +0300

    First commit

---
+++ xbase.go
@@ -0,0 +1,13 @@
+// xbase package implements functions for working with DBF files.
+package xbase
+
+const (
+	dbfId     byte = 0x03
+	headerEnd byte = 0x0D
+	fileEnd   byte = 0x1A
+)
+
+const (
+	fieldSize  = 32
+	headerSize = 32
+)
