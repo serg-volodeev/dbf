f4dbfd58dde184c1b078f3e82b80c643c8769673
serg-volodeev <serg.volodeev@gmail.com>
2021-04-18 11:24:55 +0300

    Test write records

--- writer.go
+++ writer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 
 	"golang.org/x/text/encoding"
 )
@@ -12,6 +13,7 @@ type Writer struct {
 	header   *header
 	fields   []*field
 	writer   *bufio.Writer
+	ws       io.WriteSeeker
 	buf      []byte
 	encoder  *encoding.Encoder
 	recCount uint32
@@ -25,8 +27,12 @@ type FieldInfo struct {
 }
 
 func NewWriter(ws io.WriteSeeker, fields []FieldInfo, codePage int) (*Writer, error) {
+	if len(fields) == 0 {
+		return nil, fmt.Errorf("no fields defined")
+	}
 	w := &Writer{
 		header: newHeader(),
+		ws:     ws,
 		writer: bufio.NewWriter(ws),
 	}
 	for _, f := range fields {
@@ -34,12 +40,14 @@ func NewWriter(ws io.WriteSeeker, fields []FieldInfo, codePage int) (*Writer, er
 			return nil, err
 		}
 	}
-	cm := charmapByPage(codePage)
-	if cm == nil {
-		return nil, fmt.Errorf("Unsupported code page %d", codePage)
+	if codePage > 0 {
+		cm := charmapByPage(codePage)
+		if cm == nil {
+			return nil, fmt.Errorf("unsupported code page %d", codePage)
+		}
+		w.encoder = cm.NewEncoder()
+		w.header.setCodePage(codePage)
 	}
-	w.encoder = cm.NewEncoder()
-	w.header.setCodePage(codePage)
 	return w, nil
 }
 
@@ -49,5 +57,146 @@ func (w *Writer) addField(name string, typ string, length int, dec int) error {
 		return err
 	}
 	w.fields = append(w.fields, f)
+	return nil
+}
+
+func (w *Writer) Write(record []interface{}) error {
+	if w.recCount == 0 {
+		if err := w.initWriter(); err != nil {
+			return err
+		}
+	}
+	return w.writeRecord(record)
+}
+
+func (w *Writer) initWriter() error {
+	w.header.setFieldCount(len(w.fields))
+	w.header.RecSize = w.calcRecSize()
+	if err := w.header.write(w.writer); err != nil {
+		return err
+	}
+	if err := w.writeFields(); err != nil {
+		return err
+	}
+	if err := w.writer.WriteByte(headerEnd); err != nil {
+		return err
+	}
+	w.buf = make([]byte, int(w.header.RecSize))
+	w.buf[0] = ' ' // deleted mark
+	return nil
+}
+
+func (w *Writer) calcRecSize() uint16 {
+	size := 1 // deleted mark
+	for _, f := range w.fields {
+		size += int(f.Len)
+	}
+	return uint16(size)
+}
+
+func (w *Writer) writeFields() error {
+	offset := 1 // deleted mark
+	for _, f := range w.fields {
+		f.Offset = uint32(offset)
+		if err := f.write(w.writer); err != nil {
+			return err
+		}
+		offset += int(f.Len)
+	}
+	return nil
+}
+
+func (w *Writer) writeRecord(record []interface{}) error {
+	if len(record) != len(w.fields) {
+		return fmt.Errorf("the record does not match the number of fields")
+	}
+	for i := range w.fields {
+		if err := w.setFieldValue(i, record[i]); err != nil {
+			return err
+		}
+	}
+	if _, err := w.writer.Write(w.buf); err != nil {
+		return err
+	}
+	w.recCount++
+	return nil
+}
+
+func (w *Writer) setFieldValue(index int, value interface{}) error {
+	var fieldStr string
+	f := w.fields[index]
+
+	switch f.Type {
+	case 'C':
+		v, err := interfaceToString(value)
+		if err != nil {
+			return err
+		}
+		if w.encoder != nil && !isASCII(v) {
+			v, err = w.encoder.String(v)
+			if err != nil {
+				return err
+			}
+		}
+		if err := f.checkLen(v); err != nil {
+			return err
+		}
+		fieldStr = padRight(v, int(f.Len))
+	case 'L':
+		v, err := interfaceToBool(value)
+		if err != nil {
+			return err
+		}
+		fieldStr = "F"
+		if v {
+			fieldStr = "T"
+		}
+	case 'D':
+		v, err := interfaceToDate(value)
+		if err != nil {
+			return err
+		}
+		fieldStr = v.Format("20060102")
+	case 'N':
+		var s string
+		if f.Dec == 0 {
+			v, err := interfaceToInt(value)
+			if err != nil {
+				return err
+			}
+			s = strconv.FormatInt(v, 10)
+		} else {
+			v, err := interfaceToFloat(value)
+			if err != nil {
+				return err
+			}
+			s = strconv.FormatFloat(v, 'f', int(f.Dec), 64)
+		}
+		if err := f.checkLen(s); err != nil {
+			return err
+		}
+		fieldStr = padLeft(s, int(f.Len))
+	default:
+		return fmt.Errorf("invalid field type: got %s, want C, N, L, D", string(f.Type))
+	}
+	copy(w.buf[int(f.Offset):int(f.Offset)+int(f.Len)], fieldStr)
+	return nil
+}
+
+func (w *Writer) Flush() error {
+	if err := w.writer.WriteByte(fileEnd); err != nil {
+		return err
+	}
+	if err := w.writer.Flush(); err != nil {
+		return err
+	}
+	// modify record count in header
+	if _, err := w.ws.Seek(0, 0); err != nil {
+		return err
+	}
+	w.header.RecCount = w.recCount
+	if err := w.header.write(w.ws); err != nil {
+		return err
+	}
 	return nil
 }
--- writer_test.go
+++ writer_test.go
@@ -1,7 +1,10 @@
 package xbase
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -15,4 +18,60 @@ func TestWriterNewWriter(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 2, len(w.fields))
 	require.NotNil(t, w.encoder)
+}
+
+func TestWriterWriteRecords(t *testing.T) {
+	f, err := os.Create("./testdata/test.dbf")
+	require.NoError(t, err)
+	defer f.Close()
+
+	fields := []FieldInfo{
+		{"NAME", "C", 20, 0},
+		{"FLAG", "L", 0, 0},
+		{"COUNT", "N", 5, 0},
+		{"PRICE", "N", 9, 2},
+		{"DATE", "D", 0, 0},
+	}
+	w, err := NewWriter(f, fields, 866)
+	require.NoError(t, err)
+
+	var d time.Time
+	d1 := time.Date(2021, 2, 12, 0, 0, 0, 0, time.UTC)
+
+	records := [][]interface{}{
+		{"Abc", true, 123, 123.45, d1},
+		{"", false, 0, 0.0, d},
+		{"Мышь", false, -321, -54.32, d1},
+	}
+
+	for i := range records {
+		err := w.Write(records[i])
+		require.NoError(t, err)
+	}
+
+	err = w.Flush()
+	require.NoError(t, err)
+
+	testBytes := readFile("./testdata/test.dbf")
+	goldBytes := readFile("./testdata/rec3.dbf")
+	require.Equal(t, goldBytes, testBytes)
+}
+
+func readFile(name string) []byte {
+	f, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	// ModDate
+	b[1] = 0
+	b[2] = 0
+	b[3] = 0
+	// CodePage
+	// b[29] = 0
+	return b
 }
