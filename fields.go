387774a06ffdddac9a2d876c6fdaac176d24be5c
serg-volodeev <serg-volodeev@gmail.com>
2021-04-27 10:08:55 +0300

    Fields Add, write

--- fields.go
+++ fields.go
@@ -1,7 +1,9 @@
 package dbf
 
+import "io"
+
 type Fields struct {
-	items []field
+	items []*field
 	err   error
 }
 
@@ -15,4 +17,36 @@ func (f *Fields) Count() int {
 
 func (f *Fields) Error() error {
 	return f.err
+}
+
+func (f *Fields) Add(name string, typ string, opts ...int) {
+	if f.err != nil {
+		return
+	}
+	length := 0
+	dec := 0
+	if len(opts) > 0 {
+		length = opts[0]
+	}
+	if len(opts) > 1 {
+		dec = opts[1]
+	}
+	item, err := newField(name, typ, length, dec)
+	if err != nil {
+		f.err = err
+		return
+	}
+	f.items = append(f.items, item)
+}
+
+func (f *Fields) write(w io.Writer) error {
+	offset := 1 // deleted mark
+	for _, item := range f.items {
+		item.Offset = uint32(offset)
+		if err := item.write(w); err != nil {
+			return err
+		}
+		offset += int(item.Len)
+	}
+	return nil
 }
--- fields_test.go
+++ fields_test.go
@@ -1,13 +1,43 @@
 package dbf
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewFields(t *testing.T) {
+func TestFieldsNewFields(t *testing.T) {
 	f := NewFields()
 	require.NoError(t, f.Error())
 	require.Equal(t, 0, f.Count())
+}
+
+func TestFieldsAdd(t *testing.T) {
+	f := NewFields()
+	f.Add("date", "d")
+	require.NoError(t, f.Error())
+	require.Equal(t, 1, f.Count())
+}
+
+func TestFieldsWrite(t *testing.T) {
+	f := NewFields()
+	f.Add("name", "C", 14)
+	f.Add("date", "D")
+
+	buf := bytes.NewBuffer(nil)
+	err := f.write(buf)
+
+	require.NoError(t, err)
+	b := buf.Bytes()
+	require.Equal(t, 64, len(b))
+	require.Equal(t, byte('N'), b[0])
+	require.Equal(t, byte('A'), b[1])
+	require.Equal(t, byte('M'), b[2])
+	require.Equal(t, byte('E'), b[3])
+	require.Equal(t, byte('C'), b[11])
+	require.Equal(t, byte(0), b[12])
+	require.Equal(t, byte(14), b[16])
+	require.Equal(t, byte('D'), b[32])
+	require.Equal(t, byte('A'), b[33])
 }
