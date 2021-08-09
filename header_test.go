5fdd5a90d219a287c05d360d4cfb5a275d46ad4c
serg-volodeev <serg-volodeev@gmail.com>
2021-08-09 15:05:24 +0300

    Excluded testify (header)

--- header_test.go
+++ header_test.go
@@ -2,31 +2,28 @@ package dbf
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
 )
+
+var headerBytes = []byte{0x3, 0x1e, 0x2, 0x14, 0x3, 0, 0, 0, 0xc1, 0, 0x27,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x65, 0, 0}
 
 func currentDate() time.Time {
 	y, m, d := time.Now().Date()
 	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
 }
 
-func TestHeaderNew(t *testing.T) {
+func Test_header_newHeader(t *testing.T) {
 	h := newHeader()
 
-	require.Equal(t, byte(0x03), h.Id)
-	require.Equal(t, uint32(0), h.RecCount)
-	require.Equal(t, -1, h.fieldCount())
-	require.Equal(t, uint16(0), h.RecSize)
-	require.Equal(t, currentDate(), h.modDate())
+	if h.Id != 0x03 || h.fieldCount() != -1 || h.modDate() != currentDate() {
+		t.Errorf("Error newHeader():\ngot: %#v", h)
+	}
 }
 
-var headerBytes = []byte{0x3, 0x1e, 0x2, 0x14, 0x3, 0, 0, 0, 0xc1, 0, 0x27,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x65, 0, 0}
-
-func TestHeaderWrite(t *testing.T) {
+func Test_header_write(t *testing.T) {
 	h := newHeader()
 	h.RecCount = uint32(3)
 	h.RecSize = uint16(39)
@@ -37,38 +34,58 @@ func TestHeaderWrite(t *testing.T) {
 	buf := bytes.NewBuffer(nil)
 	h.write(buf)
 
-	require.Equal(t, headerBytes, buf.Bytes())
+	if !reflect.DeepEqual(buf.Bytes(), headerBytes) {
+		t.Errorf("Error header.write():\nwant: %#v\ngot : %#v", headerBytes, buf.Bytes())
+	}
 }
 
-func TestHeaderRead(t *testing.T) {
-	r := bytes.NewReader(headerBytes)
-
+func Test_header_read(t *testing.T) {
 	h := &header{}
-	h.read(r)
+	h.read(bytes.NewReader(headerBytes))
 
-	require.Equal(t, byte(0x03), h.Id)
-	require.Equal(t, uint32(3), h.RecCount)
-	require.Equal(t, 5, h.fieldCount())
-	require.Equal(t, uint16(39), h.RecSize)
-	require.Equal(t, 866, h.codePage())
+	tpl := "header.read(): %s: want: %v, got: %v"
 
+	if h.Id != 0x03 {
+		t.Errorf(tpl, "h.Id", 0x03, h.Id)
+	}
+	if h.RecCount != 3 {
+		t.Errorf(tpl, "h.RecCount", 3, h.RecCount)
+	}
+	if h.fieldCount() != 5 {
+		t.Errorf(tpl, "h.fieldCount()", 5, h.fieldCount())
+	}
+	if h.RecSize != 39 {
+		t.Errorf(tpl, "h.RecSize", 39, h.RecSize)
+	}
+	if h.codePage() != 866 {
+		t.Errorf(tpl, "h.codePage()", 866, h.codePage())
+	}
 	d := time.Date(1930, 2, 20, 0, 0, 0, 0, time.UTC)
-	require.Equal(t, d, h.modDate())
+	if h.modDate() != d {
+		t.Errorf(tpl, "h.modDate()", d, h.modDate())
+	}
 }
 
-func TestHeaderReadNotDBF(t *testing.T) {
+func Test_header_read_notDBF(t *testing.T) {
 	b := make([]byte, headerSize)
 	b[0] = 0x05 // valid 0x03
-	r := bytes.NewReader(b)
 
 	h := &header{}
-	err := h.read(r)
-	require.Error(t, err)
+	err := h.read(bytes.NewReader(b))
+
+	if err == nil {
+		t.Errorf("header.read(): error not DBF expected")
+	}
 }
 
-func TestHeaderSetCodePage(t *testing.T) {
+func Test_header_setCodePage(t *testing.T) {
 	h := &header{}
 	h.setCodePage(866)
-	require.Equal(t, byte(0x65), h.CP)
-	require.Equal(t, 866, h.codePage())
+
+	if h.CP != 0x65 {
+		t.Errorf("header.setCodePage(866): h.CP: want: %v, got: %v", 0x65, h.CP)
+	}
+	if h.codePage() != 866 {
+		t.Errorf("header.setCodePage(866): h.codePage(): want: %v, got: %v", 866, h.codePage())
+	}
 }
