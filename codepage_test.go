f18bbc2cbc9c45a433e4a0d8ff0137def925bad4
serg-volodeev <serg-volodeev@gmail.com>
2021-08-10 09:14:43 +0300

    Excluded testify (codepage)

--- codepage_test.go
+++ codepage_test.go
@@ -3,20 +3,56 @@ package dbf
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"golang.org/x/text/encoding/charmap"
 )
 
-// New field
-
-func TestCodeByPage(t *testing.T) {
-	require.Equal(t, byte(0x65), codeByPage(866))
+func Test_сodeByPage(t *testing.T) {
+	tests := []struct {
+		page int
+		want byte
+	}{
+		{page: 866, want: 0x65},
+		{page: -1, want: 0x00},
+		{page: 1251, want: 0xC9},
+	}
+	for _, tc := range tests {
+		got := codeByPage(tc.page)
+		if got != tc.want {
+			t.Errorf("codeByPage(%#v), want: %#v, got: %#v", tc.page, tc.want, got)
+		}
+	}
 }
 
-func TestPageByCode(t *testing.T) {
-	require.Equal(t, 866, pageByCode(0x65))
+func Test_pageByCode(t *testing.T) {
+	tests := []struct {
+		code byte
+		want int
+	}{
+		{code: 0x65, want: 866},
+		{code: 0x00, want: 0},
+		{code: 0xC9, want: 1251},
+	}
+	for _, tc := range tests {
+		got := pageByCode(tc.code)
+		if got != tc.want {
+			t.Errorf("pageByCode(%#v), want: %#v, got: %#v", tc.code, tc.want, got)
+		}
+	}
 }
 
-func TestCharmapByPage(t *testing.T) {
-	require.Equal(t, charmap.CodePage866, charmapByPage(866))
+func Test_charmapByPage(t *testing.T) {
+	tests := []struct {
+		page int
+		want *charmap.Charmap
+	}{
+		{page: 866, want: charmap.CodePage866},
+		{page: -1, want: nil},
+		{page: 1251, want: charmap.Windows1251},
+	}
+	for _, tc := range tests {
+		got := charmapByPage(tc.page)
+		if got != tc.want {
+			t.Errorf("charmapByPage(%v), want: %v, got: %v", tc.page, tc.want, got)
+		}
+	}
 }
